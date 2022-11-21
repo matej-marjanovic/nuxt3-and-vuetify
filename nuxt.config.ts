e42ec16c0a4724bf6dfd0da2f827fad928ce3178
Matej Marjanovic <matej@outofgalaxy.com>
2022-11-21 17:41:56 +0100

    using next.vuetifyjs.com/en/features/treeshaking/#treeshaking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import vuetify from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig',
+      (config) => {
+        config.plugins.push(vuetify());
+      });
+    }
+  ],
   css: ["vuetify/lib/styles/main.sass", "@mdi/font/css/materialdesignicons.min.css"],
   build: {
     transpile: ["vuetify"],
