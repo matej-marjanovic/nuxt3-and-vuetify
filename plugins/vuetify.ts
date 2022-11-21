6affed7ca05a4d1d977b1623b8c38015c92d5638
Matej Marjanovic <matej@outofgalaxy.com>
2022-11-21 18:31:49 +0100

    using app.vue from free theme from vuetify

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,8 +1,49 @@
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import { VBtn } from 'vuetify/components'
+
 
 export default defineNuxtPlugin(nuxtApp => {
-  const vuetify = createVuetify({ });
+  const vuetify = createVuetify({
+    aliases: {
+      VBtnAlt: VBtn,
+    },
+    // https://next.vuetifyjs.com/features/global-configuration/
+    defaults: {
+      global: {
+        rounded: 'sm',
+      },
+      VAppBar: {
+        flat: true,
+      },
+      VBtn: {
+        color: 'primary',
+        height: 44,
+        minWidth: 190,
+      },
+      VBtnAlt: {
+        color: 'primary',
+        height: 48,
+        minWidth: 190,
+        variant: 'outlined',
+      },
+      VSheet: {
+        color: '#212121',
+      },
+    },
+    // https://next.vuetifyjs.com/features/theme/
+    theme: {
+      defaultTheme: 'dark',
+      themes: {
+        dark: {
+          dark: true,
+          colors: {
+            primary: '#1697f6',
+          },
+        },
+      },
+    },
+  });
 
   nuxtApp.vueApp.use(vuetify)
 })
